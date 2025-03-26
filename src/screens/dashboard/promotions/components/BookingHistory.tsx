bc5f2ebd73147e6ee707c5108318785034bcfe52
Ichoku Chinonso <37806189+greatnonso7@users.noreply.github.com>
2025-03-26 14:01:36 +0100

    cleaning up

--- src/screens/dashboard/promotions/components/BookingHistory.tsx
+++ src/screens/dashboard/promotions/components/BookingHistory.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import {Box, Text} from 'design-system';
 import {hp, wp} from 'utils';
-import {ScrollView} from 'react-native';
+import {ScrollView, TextInput, TouchableOpacity} from 'react-native';
+import {Icon} from 'shared';
+import theme from 'theme';
+import {styles} from './style';
 
 export const BookingHistory = () => {
   return (
     <Box mt={hp(20)}>
       <ScrollView>
-        <Box mt={hp(20)} mx={wp(16)}></Box>
+        <Box mt={hp(10)} mx={wp(16)}>
+          <Box
+            flexDirection={'row'}
+            justifyContent={'space-between'}
+            borderWidth={1}
+            as={TouchableOpacity}
+            activeOpacity={0.8}
+            borderColor={theme.colors.BASE_SECONDARY}
+            borderRadius={hp(24)}
+            px={wp(16)}
+            py={hp(10)}
+            alignSelf={'flex-start'}
+            alignItems={'center'}>
+            <Icon name="filter-icon" />
+            <Text variant="bodyMedium" px={wp(2)} color={theme.colors.WHITE}>
+              Filter by
+            </Text>
+
+            <Icon name="drop-down-icon" />
+            <Text variant="bodyBold" pl={wp(2)} color={theme.colors.WHITE}>
+              All Bookings
+            </Text>
+          </Box>
+
+          <Box mt={hp(20)} style={styles.searchInputContainer}>
+            <Icon name="search-icon" />
+            <TextInput
+              style={styles.searchTextInput}
+              placeholder="Search DJ"
+              selectionColor={theme.colors.WHITE}
+              placeholderTextColor={theme.colors.TEXT_INPUT_PLACEHOLDER}
+            />
+          </Box>
+        </Box>
       </ScrollView>
     </Box>
   );
--- src/screens/dashboard/promotions/components/style.ts
+++ src/screens/dashboard/promotions/components/style.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 import theme from 'theme';
-import {hp, wp} from 'utils';
+import {fontSz, hp, wp} from 'utils';
 
 export const styles = StyleSheet.create({
   promotionContainer: {
@@ -38,5 +38,22 @@ export const styles = StyleSheet.create({
     width: wp(30),
     height: hp(30),
     borderRadius: 100,
+  },
+  searchInputContainer: {
+    borderColor: theme.colors.OFF_WHITE_200,
+    height: hp(56),
+    borderRadius: hp(24),
+    backgroundColor: theme.colors.TEXT_INPUT_BG,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: hp(16),
+  },
+  searchTextInput: {
+    height: hp(56),
+    width: wp(250),
+    fontFamily: theme.font.DMSansRegular,
+    color: theme.colors.WHITE,
+    fontSize: fontSz(16),
+    marginLeft: wp(12),
   },
 });
